82bfc272434d0e357a02a28d754718af47917115
fidelisbae <fidelisbae@gmail.com>
2023-03-31 14:17:33 +0900

    user

---
+++ src/user/user.entity.ts
@@ -0,0 +1,27 @@
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
+import { Exam } from '../exam/exam.entity';
+
+@Entity()
+export class User {
+  @PrimaryColumn({ type: 'varchar', length: 255 })
+  id: number;
+
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  password: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  name: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  phone: string;
+
+  @Column({ type: 'boolean', nullable: false, default: false })
+  is_teacher: boolean;
+
+  @ManyToOne(() => Exam)
+  @JoinColumn({ name: 'exam_id', referencedColumnName: 'id' })
+  exam: Exam;
+
+  @Column({ type: 'int', nullable: true })
+  exam_id: number;
+}
