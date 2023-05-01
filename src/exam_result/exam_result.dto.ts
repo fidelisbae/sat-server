e0ccfd9c75c803f3c2ceddbb6ea3ebc63c31bee5
fidelisbae <fidelisbae@gmail.com>
2023-05-01 18:32:05 +0900

    allow null

--- src/exam_result/exam_result.dto.ts
+++ src/exam_result/exam_result.dto.ts
@@ -5,5 +5,5 @@ export class CreateExamResultDto {
   @ApiProperty({ description: '문제의 정답을 배열형태로 98개 입력' })
   @IsArray()
   @Length(98, 98)
-  readonly answers: string[];
+  readonly answers: (string | null)[];
 }
