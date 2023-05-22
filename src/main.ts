20eebeca68530cbb0b4334b2d76d66879fe6803d
fidelisbae <fidelisbae@gmail.com>
2023-05-23 00:34:20 +0900

    express limit

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(express.json({ limit: '1mb' }));
+  app.use(express.json({ limit: '3mb' }));
 
   app.enableCors({
     origin: 'https://brixsat.com',
