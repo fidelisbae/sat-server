7d69b8c0610c283d4e2201c30922ee94a4893538
fidelisbae <fidelisbae@gmail.com>
2023-06-04 22:14:09 +0900

    validate 수정

--- src/exam_result/exam_result.dto.ts
+++ src/exam_result/exam_result.dto.ts
@@ -1,8 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsArray } from 'class-validator';
+import { IsArray, IsNotEmpty, IsNumber } from 'class-validator';
 
 export class CreateExamResultDto {
   @ApiProperty({ description: '제출할 시험의 id' })
+  @IsNumber()
+  @IsNotEmpty()
   readonly exam_id: number;
 
   @ApiProperty({ description: '문제의 정답을 배열형태로 98개 입력' })
